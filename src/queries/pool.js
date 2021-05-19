b891aeef752ba3da51f560ac38fc3da7ed0ebb15
Dmytro Ukrainets <dimon2000.1982@gmail.com>
2021-05-19 03:12:49 +0300

    fix

--- src/queries/pool.js
+++ src/queries/pool.js
@@ -1,10 +1,11 @@
 const { Pool } = require("pg");
 //process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+console.log(process.env.DATABASE_URL);
 const db =
   process.env.NODE_ENV === "test" ? "movieplaza_test" : "dboqj906ps4mnl";
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: true,
+  ssl: { rejectUnauthorized: false },
 });
 pool.connect();
 
